c351fe8910bb147d87b0ab6617863a257ac340a1
bavla <vladimir.batagelj@gmail.com>
2023-12-26 14:28:38 +0100

    Add files via upload

--- R/izraz2024.R
+++ R/izraz2024.R
@@ -1,7 +1,18 @@
-o <- c("","+","-","*"); k <- 0
-izraz <- function(s,n){
+o <- c("","+","-","*") 
+
+izrazP <- function(s,n){
   if(n==0) {k <<- k+1; v <- eval(str2expression(s))
     if(v==2024) cat(k,s,v,"\n")
-  } else for(p in o) {sn <- paste(s,p,n,sep=""); izraz(sn,n-1)}
+  } else for(p in o) izrazP(paste(s,p,n,sep=""),n-1)
 }
-izraz("9",8); izraz("-9",8)
+
+izrazR <- function(s,n){
+  if(n==0) {t <- paste("-",s,sep="")
+    k <<- k+1; v <- eval(str2expression(s)); if(v==2024) cat(k,s,v,"\n")
+    k <<- k+1; v <- eval(str2expression(t)); if(v==2024) cat(k,t,v,"\n")
+  } else for(p in o) izrazR(paste(n,p,s,sep=""),n-1)
+}
+
+k <- 0; izrazP("9",8); izrazP("-9",8)
+k <- 0; izrazR("9",8)
+
